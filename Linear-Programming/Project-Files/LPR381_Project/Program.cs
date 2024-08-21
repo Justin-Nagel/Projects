3c0cfcaa8d46912e392f526a9913a99a81bfd7d3
Justin-Nagel <141130898+Justin-Nagel@users.noreply.github.com>
2024-08-21 14:01:24 +0200

    Add files via upload

---
+++ Linear-Programming/Project-Files/LPR381_Project/Program.cs
@@ -0,0 +1,15 @@
+﻿using Presentation;
+using System;
+
+namespace LPR381_Project
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            MainMenu.Run();
+            Console.ReadKey();
+        }
+    }
+}
